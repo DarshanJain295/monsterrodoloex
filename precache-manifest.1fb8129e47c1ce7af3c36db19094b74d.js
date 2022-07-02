789a53762229410895afa5b30a5cb0da14409441
Darshan Jain <jaindarshan295@gmail.com>
2022-07-02 18:57:06 +0530

    Updates

---
+++ precache-manifest.1fb8129e47c1ce7af3c36db19094b74d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2266168d29848271737",
+    "url": "/monsterrodoloex/static/css/main.80ad7a14.chunk.css"
+  },
+  {
+    "revision": "e2266168d29848271737",
+    "url": "/monsterrodoloex/static/js/main.e2266168.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/monsterrodoloex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "6a9ed8bb838c5d13005f",
+    "url": "/monsterrodoloex/static/js/2.6a9ed8bb.chunk.js"
+  },
+  {
+    "revision": "054bf39cc81ac9599824",
+    "url": "/monsterrodoloex/static/js/runtime~main.054bf39c.js"
+  },
+  {
+    "revision": "1a04473400a711af11905ad6e993ba63",
+    "url": "/monsterrodoloex/index.html"
+  }
+];
